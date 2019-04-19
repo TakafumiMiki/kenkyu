58686e92e60fa87ad89d2892cb85c6c65ce39d5b
MikiTakafumi <karugami02468@gmail.com>
2019-04-19 10:52:39 +0900

    add script required for modeling
    音声データのモデリングに利用するコードを追加しました。

--- mfcc.py
+++ mfcc.py
@@ -1,29 +1,51 @@
 import librosa, librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
-import glob
+import glob    
 
 def visualize(sig, fs):
-    data = []
     melspecs = librosa.feature.melspectrogram(y=sig, sr=fs,
-                                            n_fft=2048, n_mels=128)
-    data.extend(melspecs)
-    """
-    librosa.display.specshow(librosa.power_to_db(melspecs, ref=np.max),
-                            x_axis='time', y_axis='mel', fmax=fs)
+                                            n_fft=2048, n_mels=100)
+    
+    # Vlibrosa.display.specshow(librosa.power_to_db(melspecs, ref=np.max),
+    #                        x_axis='time', y_axis='mel', fmax=fs)
+    
+    # plt.colorbar(format='%+2.0f dB')
+    # plt.show()
+    
+    return melspecs
 
-    plt.colorbar(format='%+2.0f dB')
-    plt.show()
-    """
-    return data
 get_file_index = glob.glob("audio[0-9]*.wav")
+TEST_DATA_NUM = len(get_file_index) // 2
+TRAIN_DATA_NUM = len(get_file_index) - TEST_DATA_NUM
+# print(TRAIN_DATA_NUM)
+# print(TEST_DATA_NUM)
 
-for i in get_file_index:
-    sig, fs = librosa.audio.load(i)
-    data = visualize(sig, fs)
-    # dataは1つあたり128次元 (上のn_melsで決定) 
-    print(len(data))
+try:
+    if TRAIN_DATA_NUM > 3 or TEST_DATA_NUM > 3:
+        tra_data = []
+        pred_data = []
 
-# svmしやすい形で保存したい
-# with open("ashioto.txt", mode='w') as f:
-#     f.writelines(data)
+        for i in range(TRAIN_DATA_NUM):
+            sig, fs = librosa.audio.load(get_file_index[i])
+            tra_data.append(visualize(sig, fs))
+            # data = np.append(data, visualize(sig, fs))
+
+        for j in range(TRAIN_DATA_NUM, len(get_file_index)):
+            sig, fs = librosa.audio.load(get_file_index[-j])
+            pred_data.append(visualize(sig, fs))
+
+        # print(len(data), len(pred_data))
+
+        # if data[-1] is pred_data[0]:
+        #     print("missing")
+
+        # else:
+        #     print("ok")
+
+    else:
+        raise ValueError
+
+except ValueError:
+    print("データ数が足りません")
+    
--- rokuon.py
+++ rokuon.py
@@ -2,21 +2,12 @@ import pyaudio
 import wave
 import glob
 
+
 # 録音時間
-RECORD_SECOND = 5
+RECORD_SECOND = 2
 
 # 録音デバイスの番号
 iDeviceIndex = 0
-
-get_file_index = glob.glob("audio[0-9]*.wav")
-get_file_index.sort(reverse = True)
-
-if len(get_file_index) != 0:
-    max_index = ''.join(c for c in get_file_index[0] if c.isdigit())
-    this_index = int(max_index) + 1
-
-else:
-    this_index = 1
 
 # 録音情報の設定
 FORMAT = pyaudio.paInt16 #音声のフォーマット
@@ -24,7 +15,6 @@ CHANNELS = 2
 RATE = 44100             #サンプルレート
 CHUNK = 1024            
 audio = pyaudio.PyAudio()
-
 stream = audio.open(format=FORMAT, channels=CHANNELS,
         rate=RATE, input=True,
         input_device_index = iDeviceIndex, #録音デバイスのインデックス番号
@@ -40,10 +30,26 @@ print("finished recording")
 stream.stop_stream()
 stream.close()
 audio.terminate()
- 
-waveFile = wave.open("audio"+ str(this_index) + ".wav", 'w')
-waveFile.setnchannels(CHANNELS)
-waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-waveFile.setframerate(RATE)
-waveFile.writeframes(b''.join(all))
-waveFile.close()
+
+def get_file_index():
+    file_index = glob.glob("audio[0-9]*.wav")
+    file_index.sort(reverse = True)
+    if len(file_index) != 0:
+        max_index = ''.join(c for c in file_index[0] if c.isdigit())
+        this_index = int(max_index) + 1
+
+    else:
+        this_index = 1
+
+    return this_index
+
+def get_audio():
+    this_index = get_file_index()
+    waveFile = wave.open("audio"+ str(this_index) + ".wav", 'w')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(all))
+    waveFile.close()
+
+get_audio()
